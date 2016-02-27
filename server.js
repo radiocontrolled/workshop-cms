e7cd2571df596dc7c7ce132b968a3a52b6f0a3d3
radiocontrolled <radiocontrolled@gmail.com>
2016-02-27 16:40:40 +0000

    save

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 var http = require("http");
+var fs = require("fs");
 
 
 function handler(request, response) {
-  response.writeHead(200, {"Content-Type" : "Text/html"});
-  response.write("hey there");
-  response.end();
+
+  var endpoint = request.url; 
+
+  if(endpoint === "/") {
+    response.writeHead(200, {"Content-Type" : "Text/html"});
+    fs.readFile(__dirname + "/public/index.html", function(error, file){
+      if(error) {
+        console.log(error); return; 
+      }
+      response.end(file);
+    });
+  
+  }
+
+  else {
+    if(endpoint.indexOf(".css") > -1) {
+      response.writeHead(200, {"Content-Type" : "Text/css"});
+
+    }
+
+    if(endpoint.indexOf(".png") > -1) {
+      response.writeHead(200, {"Content-Type" : "image/png"});
+  
+    }
+
+     fs.readFile(__dirname + "/public" + endpoint, function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      response.end(file);
+    
+    });
+  }
+
+
+
 }
 
 var server = http.createServer(handler); 
