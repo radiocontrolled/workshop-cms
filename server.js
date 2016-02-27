5cf4feeb0669a2c76e77e9f206b44f20dde2feab
Tracy Wu <sytracywu@gmail.com>
2016-02-27 13:15:08 +0000

    step4 we served our first html page

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 var http = require('http');
-var message = "I am so happy to be a part of the Node girls workshop";
+var fs = require('fs');
 
-// handler takes the request from the client, and the response object sends a message back to the client. 
-function handler(request, response) {
-	response.writeHead(200, {"Content-Type": "text/html"}); //this is the header
-	response.write(message); //this is the response body
-	response.end(); //finish response
+function handler (request, response) {
 
+	if (request.url === "/") {
+
+		fs.readFile(__dirname + '/public/index.html', function(error, file) {
+			if (error) {
+				console.log(error);
+				return;
+			}
+			else {
+				response.writeHead(200, {"Content-Type": "text/html"});
+			}
+			response.end(file);
+		});
+	}
 }
+
+
+// var http = require('http');
+// var message = "I am so happy to be a part of the Node girls workshop";
+
+// // handler takes the request from the client, and the response object sends a message back to the client. 
+// function handler(request, response) {
+// 	var method = request.method;
+// 	console.log(request); //this is going to tell us which http method we're usin
+// 	var endpoint = request.url; 
+// 	console.log(endpoint);
+
+// 	if (endpoint === "/node") {
+// 		response.write("looking at a node page");
+
+// 	} else {
+// 		response.writeHead(200, {"Content-Type": "text/html"}); //this is the header
+// 		response.write(message); //this is the response body
+// 	}
+// response.end(); //finish response
+// }
 
 var server = http.createServer(handler); // our server
 
