80fe639b15f26510977bad284f8f1f7cb3d92952
radiocontrolled <radiocontrolled@gmail.com>
2016-02-27 14:43:26 +0000

    step5

--- server.js
+++ server.js
@@ -3,19 +3,69 @@ var fs = require('fs');
 
 function handler (request, response) {
 
-	if (request.url === "/") {
+  var endpoint = request.url; 
 
-		fs.readFile(__dirname + '/public/index.html', function(error, file) {
-			if (error) {
-				console.log(error);
-				return;
-			}
-			else {
-				response.writeHead(200, {"Content-Type": "text/html"});
-			}
-			response.end(file);
-		});
-	}
+  if (endpoint === '/') {
+
+    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+      if (error) {
+       console.log(error);
+       return;
+      }
+      else {
+       response.writeHead(200, {"Content-Type": "text/html"});
+      }
+      response.end(file);
+    });
+  } 
+
+  else if (endpoint === '/node') {
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("This is node");
+    response.end();
+
+  } 
+
+  else if (endpoint === '/girls') {
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("This is girls");
+    response.end();
+
+  } 
+
+  else {
+  
+
+    if(endpoint.indexOf(".css") > -1 ) {
+      response.writeHead(200, {"Content-Type": "text/css"});
+
+    }
+    
+    if(endpoint.indexOf(".jpg") > -1 ) {
+      response.writeHead(200, {"Content-Type": "image/jpg"});
+    }
+
+    if(endpoint.indexOf(".ico") > -1 ) {
+      response.writeHead(200, {"Content-Type": "image/x-icon"});
+    }
+
+    fs.readFile(__dirname + '/public' + endpoint, function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      response.end(file);
+    
+    });
+
+  }
+
+
+
+	
 }
 
 
