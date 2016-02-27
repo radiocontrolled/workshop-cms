a9c49d1fc49be8a8f45cc142918f32136c624d10
radiocontrolled <radiocontrolled@gmail.com>
2016-02-27 11:53:17 +0000

    we have created a server

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer(); // our server
+
+// set a port for our server to listen to requests with
+
+server.listen(3000, function() {
+ console.log("Server is listening on port 3000. ready to accept requests");
+});
+
+
+
