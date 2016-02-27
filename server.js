e6fac0f7a9e95d23877930a55c9adf9c48e88244
Tracy Wu <sytracywu@gmail.com>
2016-02-27 12:15:31 +0000

    saving step 3

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var message = "I am so happy to be a part of the Node girls workshop";
 
 // handler takes the request from the client, and the response object sends a message back to the client. 
 function handler(request, response) {
+	response.writeHead(200, {"Content-Type": "text/html"}); //this is the header
+	response.write(message); //this is the response body
+	response.end(); //finish response
 
 }
 
-var server = http.createServer(); // our server
+var server = http.createServer(handler); // our server
 
 // set a port for our server to listen to requests with
 
